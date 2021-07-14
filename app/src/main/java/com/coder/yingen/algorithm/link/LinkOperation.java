3a601b3f0c36293eb55d4879cd14227d01267418
chuyingen14042 <chuyingen14042@ipalfish.com>
2021-07-14 19:11:30 +0800

    k个一组 翻转链表

--- app/src/main/java/com/coder/yingen/algorithm/link/LinkOperation.java
+++ app/src/main/java/com/coder/yingen/algorithm/link/LinkOperation.java
@@ -9,6 +9,7 @@ public class LinkOperation {
 
     /**
      * 链表翻转
+     *
      * @param head
      * @return
      */
@@ -32,14 +33,51 @@ public class LinkOperation {
         return preNode;
     }
 
-    public static int getLength(ListNode head){
-        int n =0 ;
+    public static int getLength(ListNode head) {
+        int n = 0;
         ListNode node = head;
         while (node != null) {
             node = node.next;
             n++;
         }
         return n;
+    }
 
+    /**************************************************K 个一组 翻转链表************************************************/
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode pre = dummy;
+        ListNode end = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+            if (end == null) break;
+            ListNode start = pre.next;
+            ListNode next = end.next;
+            end.next = null;
+            pre.next = reverse(start);
+            start.next = next;
+            pre = start;
+            end = pre;
+        }
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode start) {
+        if (start == null || start.next == null){
+            return start;
+        }
+        ListNode pre = null;
+        ListNode curr = start;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+        return pre;
     }
 }
